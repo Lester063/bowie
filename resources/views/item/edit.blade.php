d698f22679d03a142a824409490fdc7320a49dc4
Lester063 <102047666+Lester063@users.noreply.github.com>
2023-03-27 22:43:57 -0600

    admin select option
    remove other option, add condition when the status of item is available.

--- resources/views/item/edit.blade.php
+++ resources/views/item/edit.blade.php
@@ -7,10 +7,12 @@
                 <x-splade-form :default="$item" method="PUT" :action="route('item.update',$item->id)">
                     <x-splade-input name="item_name" label="Item name" />
                     <x-splade-input name="item_code" label="Item code" />
+                    
                     <x-splade-select name="status" class="mt-3">
-                        <option value="PENDING">PENDING</option>
-                        <option value="PROCESSING">PROCESSING</option>
-                        <option value="PROCESSED">PROCESSED</option>
+                        <option value="{{$item->status}}">{{$item->status}}</option>
+                        @if($item->status!='AVAILABLE')
+                        <option value="AVAILABLE">AVAILABLE</option>
+                        @endif
                     </x-splade-select>
                     
                     <x-splade-submit class="mt-3" />
